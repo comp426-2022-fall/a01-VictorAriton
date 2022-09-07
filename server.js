ecdadac249213b1b5fae9918e2e4d55c0d626987
VictorAriton <69809296+VictorAriton@users.noreply.github.com>
2022-09-07 14:43:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const argv = minimist(process.argv.slice(2));
 
 // Define a const port using the argument from the command line.
 // Make this const default to port 3000 if there is no argument given for --port.
-const port = argv.port || 5555;
+const port = argv.port || 3000;
 
 // Use the fs module to create an arrow function using fs.readFile.
 // Use the documentation for the Node.js fs module.
@@ -48,7 +48,7 @@ fs.readFile('./public/index.html', (err, data) => {
 // Start the server const listening on the port defined by argument in your port const.
 // Put the exact message Server listening on port ${port} on the console log.
     server.listen(port, () => {
-        console.log(Server listening on port ${port});
+        console.log('Server listening on port ${port}');
     });
 
 });
